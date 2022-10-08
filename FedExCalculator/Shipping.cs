11fbc83907d946e5aef643e0e775529e8db55ff8
Vinay Gunnam <vinay.gunnam@morningstar.com>
2022-10-08 15:37:22 -0500

    Initial commit

---
+++ FedExCalculator/Shipping.cs
@@ -0,0 +1,11 @@
+﻿namespace FedExCalculator;
+public class Shipping
+{
+  public decimal ComputeCost(decimal weightInPounds)
+  {
+    if (weightInPounds <= 0) return 0;
+    if (weightInPounds <= 5) return 6;
+    if (weightInPounds <= 10) return 15;
+    return 20;
+  }
+}
