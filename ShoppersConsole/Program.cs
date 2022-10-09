acd5876092fcc09cc8b9bc4c3c1b58c827703663
hareepg <kakarla.pravi@gmail.com>
2022-10-09 16:10:23 -0500

    Config for Console App

--- ShoppersConsole/Program.cs
+++ ShoppersConsole/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Setup Host
+        var host = CreateDefaultBuilder().Build();
+
+        // Invoke Worker
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        IServiceProvider provider = serviceScope.ServiceProvider;
+
+        var config = provider.GetService<IConfiguration>();
+        System.Console.WriteLine(config.GetValue<string>("Fedex:ApiKey"));
+
+        host.Run();
+    }
+
+    static IHostBuilder CreateDefaultBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(app =>
+            {
+                app.AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices(services =>
+            {
+                // services.AddSingleton<SomeClass>();
+            });
+    }
+}
